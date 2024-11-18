6785d3cacad4f446db9f673ff6499ed581ffec0b
FinnaginWantland <fwantland50@gmail.com>
2024-11-18 15:55:11 -0800

    Update plant.cpp

--- plant.cpp
+++ plant.cpp
@@ -1,9 +1,4 @@
-/*
- * Name: Finnagin Wantland, 2002196609, 1001, Assignment 5
- * Description: Assignment 5 Ash goes Fishing
- * Input: csv file and command line arguements
- * Output: end of the simulation data
- */
+
 #include "plant.h"
 #include <iostream>
 using namespace std;
